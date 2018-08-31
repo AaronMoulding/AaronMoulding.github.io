e7d7c332490a1134a3502d3a283b59f440fc60c8
Aaron Moulding <aaronmoulding09@gmail.com>
2018-08-31 13:11:43 +0200

    firstUpdate

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="Aaron Moulding">
+
+  <title>Aaron Moulding</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+  <link rel="stylesheet" href="./css/global.css">
+  <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+  <div class="top">
+    <a id="head-text">Aaron Moulding</a><br>
+    <a>All my stuff right in one place</a><br><br>
+    <a href="./index.php" style="padding:0 5px 0 5px">Home</a>
+  </div>
+
+  <div class="container-small"><br>
+    <a><b>Windows:</b></a>
+    <div id="windows">
+      <a>&nbsp;&nbsp;&nbsp;</a><a><b>Games:</b></a><br>
+      <div class="sub">
+        <a href="./projects/minesweeper.php">Minesweeper Reloaded</a><br><br>
+      </div>
+
+      <a>&nbsp;&nbsp;&nbsp;</a><a><b>Random:</b></a><br>
+      <div class="sub">
+        <a href="./projects/artGen3D.php">3D Art Generator</a><br>
+        <a>Bacterial Infection</a><br>
+        <a>Circle Image Generator</a><br>
+        <a>Midi Visualizer</a><br>
+        <a>Purple Rain</a><br><br>
+      </div>
+
+
+        <a>&nbsp;&nbsp;&nbsp;</a><a><b>Processing:</b></a><br>
+    <div class="sub">
+        <a href="./projects/artGen3D.php">3D Art Generator</a><br>
+        <a>Bacterial Infection</a><br>
+        <a>Circle Image Generator</a><br>
+        <a>Midi Visualizer</a><br>
+        <a href="./projects/minesweeper.php">Minesweeper Reloaded</a><br>
+        <a>Purple Rain</a><br><br>
+    </div>
+    </div>
+    <a><b>Mac:</b></a>
+    <div id="mac">
+      <a>&nbsp;&nbsp;&nbsp;</a><a>Nothing has been released for mac yet.</b></a><br><br>
+    </div>
+    <a><b>Linux:</b></a>
+    <div id="linux">
+      <a>&nbsp;&nbsp;&nbsp;</a><a>Nothing has been released for linux yet.</a><br><br>
+    </div>
+  </div>
+</body>
+</html>
---
+++ projects/minesweeper.php
@@ -0,0 +1,83 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="Aaron Moulding">
+
+  <title>Aaron Moulding</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+  <link rel="stylesheet" href="../css/global.css">
+  <link rel="stylesheet" href="../css/index.css">
+</head>
+
+<body>
+  <div class="top">
+    <a id="head-text">Aaron Moulding</a><br>
+    <a>All my stuff right in one place</a><br><br>
+    <a href="../index.php" style="padding:0 5px 0 5px">Home</a>
+  </div>
+
+  <br><div class="title"><a>Minesweeper: Reloaded</a></div>
+
+  <div class="container-big"><br>
+    <a><b>Info:</b></a>
+    <div id="info">
+      <a>&nbsp;&nbsp;&nbsp;</a><a><b>General info:</b></a><br>
+      <div class="sub">
+        <a>I made minesweeper reloaded to test out Processing. I had just recently installed<br></a>
+        <a>processing and I made a few small projects, but I have a big interest in<br></a>
+        <a>game development, so I decided to make a pretty easy game, and try to do it<br></a>
+        <a>Under 4 hours, I also succeeded at this challenge</a><br><br>
+      </div>
+
+      <a>&nbsp;&nbsp;&nbsp;</a><a><b>Version info:</b></a><br>
+      <div class="sub">
+        <a>Version: r1.0.0</a><br>
+        <a>This is a release version, and will probably not get any other updates</a><br><br>
+      </div>
+
+      <a>&nbsp;&nbsp;&nbsp;</a><a><b>Development info:</b></a><br>
+      <div class="sub">
+        <a>The development of Minesweeper Reloaded started at 28/4/2018, and also ended on the same date</a><br>
+        <a>It took about 3 hours to make this "game" and was completely made by me, but with inspiration</a><br>
+        <a>from ofcourse minesweeper and <a href="https://www.youtube.com/watch?v=LFU5ZlrR21E">The Coding Train Coding Challenge #71</a></a>
+      </div>
+      <br>
+    </div>
+
+    <div id="download">
+      <a><b>Download:</b></a><br>
+      <a>&nbsp;&nbsp;&nbsp;</a><a><b>Download links:</b></a><br>
+      <div class="sub">
+        <a class="link" target="_blank" href="https://mega.nz/#!kyIS2CYY!qXh3iCXufslJwyidXHJ7trmAPtQvEWAV2zX4xo8OphM">32bit-purpleRain-v1.0.0.zip</a><a style="color:#aaa; font-size:10px;"> Uploaded: 7:54PM 12/5/18</a><br>
+        <a class="link" target="_blank" href="https://mega.nz/#!MrJX0BqY!4-trW-K5lwPCLj193BVlRLjA2MJwSyIpHg6AVByg2v8">64bit-purpleRain-v1.0.0.zip</a><a style="color:#aaa; font-size:10px;"> Uploaded: 7:54PM 12/5/18</a><br><br>
+      </div>
+    </div>
+
+    <div id="images">
+      <a><b>Images:</b></a><br>
+      <div class="sub">
+        <img src="../img/minesweeper/1.jpg" width="40 %"/><br>
+        <a>An empty minefield.</a><br><br>
+
+        <img src="../img/minesweeper/2.jpg" width="40 %"/><br>
+        <a>Starting out...</a><br><br>
+
+        <img src="../img/minesweeper/3.jpg" width="40 %"/><br>
+        <a>Flagging some bombs...</a><br><br>
+
+        <img src="../img/minesweeper/4.jpg" width="40 %"/><br>
+        <a>Flagging some more...</a><br><br>
+
+        <img src="../img/minesweeper/5.jpg" width="40 %"/><br>
+        <a>And I died.</a><br><br>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
